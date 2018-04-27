162b081aebfcb0ad3994fbfcb09f0e1419332045
Boris Shishov <borisshishov@gmail.com>
2018-04-27 11:40:51 +0200

    Main menu, game over

---
+++ Assets/Scripts/UI/UIGameOver.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    [RequireComponent(typeof(UICanvasGroupFader))]
+    public class UIGameOver : Singleton<UIGameOver>
+    {
+        private UICanvasGroupFader _fader;
+
+        void Start()
+        {
+            _fader = GetComponent<UICanvasGroupFader>();
+        }
+
+        public void LoadScene(string scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+        public void Show()
+        {
+            Time.timeScale = 0f;
+            _fader.FadeIn();
+        }
+
+        public void Hide()
+        {
+            Time.timeScale = 1f;
+            _fader.FadeOut();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    public class UIMainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("main");
+        }
+
+        public void DontPlay()
+        {
+            Application.Quit();
+        }
+    }
+}
